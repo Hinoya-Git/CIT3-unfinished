049c54c1cd7e2cddcdcc2324f3a9ecddb754af8e
Hinoya-Git <iancharles0608@gmail.com>
2026-04-07 17:31:15 +0800

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
 
 export default defineConfig(({mode}) => {
+  base: '/CIT3-unfinished/',
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [react(), tailwindcss()],
